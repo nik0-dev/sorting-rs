8cb7cf0f772f7046af238b169af5d87941640ff9
nik0-dev <121070808+nik0-dev@users.noreply.github.com>
2024-04-28 23:19:39 -0500

    added visualization

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,13 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::rect::Rect;
 use std::time::Duration;
-
 use rand::seq::SliceRandom;
 
 const SCREEN_WIDTH: u32 = 600;
 const SCREEN_HEIGHT: u32 = 600;
 const ARRAY_SIZE : usize = 600;
+
+
 
 pub fn main() {
     
@@ -27,7 +28,6 @@ pub fn main() {
     let mut canvas = window.into_canvas().build().unwrap();
     let mut event_pump = sdl_context.event_pump().unwrap();
     
-
     let mut array: [i32; ARRAY_SIZE] = [0; ARRAY_SIZE];
     for (index, element) in array.iter_mut().enumerate() {
         *element = (index + 1) as i32;
@@ -39,12 +39,29 @@ pub fn main() {
     let min_value = *array.iter().min().unwrap();
     let max_value = *array.iter().max().unwrap();
 
-    'running: loop {   
+
+    let mut color_table : [Color; 6] = [Color::RED, Color::RGB(255, 160, 0), Color::YELLOW, Color::GREEN, Color::BLUE, Color::RGB(255, 255, 0)];
+
+    'running: loop {       
+
         canvas.set_draw_color(Color::BLACK);
         canvas.clear();
-        
+
+        for (index, &value) in array.iter().enumerate() {
+            let frac = inv_lerp(min_value as f64, max_value as f64, value as f64);
+            let bar_px_height: u32 = lerp(0 as f64, SCREEN_HEIGHT as f64, frac) as u32;
+            
+            let color_table_i = lerp(0 as f64, 5 as f64, frac) as usize;
+            canvas.set_draw_color(color_table[color_table_i]);
+
+            canvas.fill_rect(Rect::new(index as i32 * bar_px_width, (SCREEN_HEIGHT - bar_px_height) as i32, bar_px_width as u32, bar_px_height)).unwrap();
+        }
+
+        canvas.present();
+
         for event in event_pump.poll_iter() {
             match event {
+                
                 Event::Quit {..} |
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
                     break 'running
@@ -54,20 +71,72 @@ pub fn main() {
                     array.shuffle(&mut rand::thread_rng());
                 },
 
+                Event::KeyDown { keycode: Some(Keycode::Space), .. } => { 
+                    
+
+                    /* bubble */
+                    
+                    // let n = array.len();
+                    // for i in 0..n {
+                    //     for j in 0..n-i-1 {
+                    //         if array[j] > array[j+1] {
+                    //             array.swap(j, j+1);
+                
+                    //             canvas.set_draw_color(Color::BLACK);
+                    //             canvas.clear();
+                                
+                    //             for (index, &value) in array.iter().enumerate() {
+                    //                 let frac = inv_lerp(min_value as f64, max_value as f64, value as f64);
+                    //                 let bar_px_height: u32 = lerp(0 as f64, SCREEN_HEIGHT as f64, frac) as u32;
+                                    
+                    //                 let color_table_i = lerp(0 as f64, 5 as f64, frac) as usize;
+                    //                 canvas.set_draw_color(color_table[color_table_i]);
+                                    
+                    //                 canvas.fill_rect(Rect::new(index as i32 * bar_px_width, (SCREEN_HEIGHT - bar_px_height) as i32, bar_px_width as u32, bar_px_height)).unwrap();
+                    //             }
+
+                    //             canvas.present();
+                    //             std::thread::sleep(Duration::new(0, 100_000));
+                    //         }
+                    //     }
+                    // }
+
+                    /* selection */
+                        
+                    let n = array.len();
+                    for i in 0..n {
+                        canvas.set_draw_color(Color::BLACK);
+                        canvas.clear();
+                        
+                        let mut min_index = i;
+                        for j in (i + 1)..n {
+                            if array[j] < array[min_index] {
+                                min_index = j;
+                            }
+                        }
+                        if min_index != i {
+                            array.swap(i, min_index);
+                        }
+
+                        for (index, &value) in array.iter().enumerate() {
+                            let frac = inv_lerp(min_value as f64, max_value as f64, value as f64);
+                            let bar_px_height: u32 = lerp(0 as f64, SCREEN_HEIGHT as f64, frac) as u32;
+                            
+                            let color_table_i = lerp(0 as f64, 5 as f64, frac) as usize;
+                            canvas.set_draw_color(color_table[color_table_i]);
+                            
+                            canvas.fill_rect(Rect::new(index as i32 * bar_px_width, (SCREEN_HEIGHT - bar_px_height) as i32, bar_px_width as u32, bar_px_height)).unwrap();
+                        }
+                        canvas.present();
+                        std::thread::sleep(Duration::new(0, 500_000));
+                    }
+
+                },
+
                 _ => {}
             }
         }
-        
-        for (index, &value) in array.iter().enumerate() {
-            let frac = inv_lerp(min_value as f64, max_value as f64, value as f64);
-            let bar_px_height: u32 = lerp(0 as f64, SCREEN_HEIGHT as f64, frac) as u32;
-            
-            canvas.set_draw_color(Color::GRAY);
-            canvas.fill_rect(Rect::new(index as i32 * bar_px_width, (SCREEN_HEIGHT - bar_px_height) as i32, bar_px_width as u32, bar_px_height)).unwrap();
-        }
 
-        canvas.present();
-        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
 
